ee47d6835243db01c8647fe7550bf11426e70e9d
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2018-11-23 09:12:41 +0100

    Created tools for spectrum plot.

--- pysigview/plugins/measurement.py
+++ pysigview/plugins/measurement.py
@@ -20,7 +20,8 @@ United States
 # Third party imports
 import numpy as np
 from PyQt5.QtCore import pyqtSignal, Qt
-from PyQt5.QtWidgets import QVBoxLayout, QWidget
+from PyQt5.QtWidgets import (QVBoxLayout, QWidget, QTabWidget, QLineEdit,
+                             QFormLayout)
 from vispy import scene
 from vispy.scene import Line, AxisWidget, Spectrogram
 from vispy.scene.cameras import PanZoomCamera
@@ -45,7 +46,11 @@ class SignalWidget(QWidget):
         self.curr_pc = None
         self.sig_start = None
         self.sig_stop = None
-        self.spect_type = 'spectrogram' # spectrum, spectrogram
+        self.spect_type = 'spectrum'  # spectrum, spectrogram
+
+        # Spectrum variables
+        self.low_lim = None
+        self.high_lim = None
 
         # Setup camera
         self.signal_camera = PanZoomCamera()
@@ -56,58 +61,50 @@ class SignalWidget(QWidget):
 
         # TODO: change this to grid with axes and signal transform
         self.view_grid = self.canvas.central_widget.add_grid(margin=10)
-        self.view_grid.spacing = 10
 
         # Signal
-        self.signal_view = self.view_grid.add_view(row=0, col=1,
+        self.signal_view = self.view_grid.add_view(row=0, col=1, row_span=2,
                                                    camera=self.signal_camera)
         self.signal_yaxis = AxisWidget(orientation='left',
                                        axis_label='Amplitude',
                                        axis_font_size=12,
-                                       axis_label_margin=50,
                                        tick_label_margin=5)
-        self.signal_yaxis.width_max = 80
-        self.view_grid.add_widget(self.signal_yaxis, row=0, col=0)
+        self.signal_yaxis.width_max = 60
+        self.view_grid.add_widget(self.signal_yaxis, row=0, col=0, row_span=2)
 
         self.signal_xaxis = scene.AxisWidget(orientation='bottom',
                                              axis_label='Time [s]',
                                              axis_font_size=12,
-                                             axis_label_margin=50,
                                              tick_label_margin=5)
 
-        self.signal_xaxis.height_max = 80
-        self.view_grid.add_widget(self.signal_xaxis, row=1, col=1)
+        self.signal_xaxis.height_max = 55
+        self.view_grid.add_widget(self.signal_xaxis, row=2, col=1)
 
         self.signal_yaxis.link_view(self.signal_view)
         self.signal_xaxis.link_view(self.signal_view)
 
         # Spectrum
-        self.spectrum_view = self.view_grid.add_view(row=2, col=1,
+        self.spectrum_view = self.view_grid.add_view(row=3, col=1, row_span=2,
                                                      camera=self.
                                                      spectrum_camera)
 
         self.spectrum_yaxis = AxisWidget(orientation='left',
                                          axis_label='Amplitude',
                                          axis_font_size=12,
-                                         axis_label_margin=50,
                                          tick_label_margin=5)
-        self.spectrum_yaxis.width_max = 80
-        self.view_grid.add_widget(self.spectrum_yaxis, row=2, col=0)
+        self.spectrum_yaxis.width_max = 60
+        self.view_grid.add_widget(self.spectrum_yaxis, row=3, col=0,
+                                  row_span=2)
 
         self.spectrum_xaxis = scene.AxisWidget(orientation='bottom',
                                                axis_label='Frequency [Hz]',
-                                               axis_font_size=12,
-                                               axis_label_margin=50,
-                                               tick_label_margin=5)
+                                               axis_font_size=12)
 
-        self.spectrum_xaxis.height_max = 80
-        self.view_grid.add_widget(self.spectrum_xaxis, row=3, col=1)
+        self.spectrum_xaxis.height_max = 55
+        self.view_grid.add_widget(self.spectrum_xaxis, row=5, col=1)
 
         self.spectrum_yaxis.link_view(self.spectrum_view)
         self.spectrum_xaxis.link_view(self.spectrum_view)
-
-#        self.transform_view = self.canvas.central_widget.add_view(
-#                    camera=self.camera)
 
         self.signal_line = Line(parent=self.signal_view.scene, width=1)
         self.spectrum_line = Line(parent=self.spectrum_view.scene, width=1)
@@ -124,13 +121,26 @@ class SignalWidget(QWidget):
         # Set the whole layout
         self.setLayout(layout)
 
+    def set_spect_type(self, stype):
+        self.spect_type = stype
+        if stype == 'spectrum':
+            self.spectrum_line.visible = True
+            self.spectrogram.visible = False
+        elif stype == 'spectrogram':
+            if self.spectrogram:  # TODO: only temporary
+                self.spectrogram.visible = True
+            self.spectrum_line.visible = False
+
+        self.update_signals()
+
     def recieve_input(self, event):
         if event.type == 'key_press' and event.key == 'control':
             self.measurement_mode = True
-        elif event.type == 'key_release' and event.key == 'control':
-            self.measurement_mode = False
             self.sig_start = None
             self.sig_stop = None
+        elif event.type == 'key_release' and event.key == 'control':
+            self.measurement_mode = False
+
 
         # TODO: this code is duplicated here, annotations, signal_view widget - carry it out only once!!!
 
@@ -215,18 +225,32 @@ class SignalWidget(QWidget):
         self.signal_line.set_data(pos=pos, color=self.curr_pc.line_color)
         self.signal_line.transform = transform
 
-        r = self.signal_camera.rect
-        r.left = 0
-        r.right = len(data) * s_x
-        r.bottom = 0
-        r.top = np.max(data)-np.min(data)
-        self.signal_camera.rect = r
+        self.signal_camera.rect = (0, 0), (len(data) * s_x,
+                                           np.max(data)-np.min(data))
 
         if self.spect_type == 'spectrum':
             # Spectrum line
 
             s = np.abs(np.fft.fft(data))[:int(len(data)/2)]
             s[0] = 0
+            freqs = np.fft.fftfreq(data.size, 1 / self.curr_pc.fsamp)
+            freqs = freqs[:int(len(freqs)/2)]
+
+            if self.low_lim is not None:
+                res = np.where(freqs >= self.low_lim)[0]
+                if len(res) > 0:
+                    low_lim_idx = res[0]
+            else:
+                low_lim_idx = 0
+
+            if self.high_lim is not None:
+                res = np.where(freqs <= self.high_lim)[0]
+                if len(res) > 0:
+                    high_lim_idx = res[-1]
+            else:
+                high_lim_idx = len(freqs)
+
+            freqs = freqs[low_lim_idx:high_lim_idx]
 
             s_x = (self.curr_pc.fsamp / 2) / len(s)
             s_y = 1
@@ -240,12 +264,11 @@ class SignalWidget(QWidget):
             self.spectrum_line.set_data(pos=pos, color=self.curr_pc.line_color)
             self.spectrum_line.transform = transform
 
-            r = self.spectrum_camera.rect
-            r.left = 0
-            r.right = self.curr_pc.fsamp / 2
-            r.bottom = 0
-            r.top = np.max(s)
-            self.spectrum_camera.rect = r
+            pos = (freqs[0], 0)
+            size = (freqs[-1] - freqs[0],
+                    np.max(s[low_lim_idx:high_lim_idx]))
+
+            self.spectrum_camera.rect = pos, size
 
         elif self.spect_type == 'spectrogram':
 
@@ -256,6 +279,7 @@ class SignalWidget(QWidget):
             color_scale = 'log'
             cmap = 'viridis'
             # TODO: this is only temporary - create my onw spectrogram
+            # TODO change axis lables
             if self.spectrogram is not None:
                 self.spectrogram.visible = False
             self.spectrogram = Spectrogram(data,
@@ -268,7 +292,7 @@ class SignalWidget(QWidget):
                                            parent=self.spectrum_view.scene)
             freqs = self.spectrogram.freqs
             n_windows = (len(data) - n_fft) // step + 1
-            s_x = 1
+            s_x = (len(data) / n_windows)/self.curr_pc.fsamp
             s_y = (self.curr_pc.fsamp / 2) / len(freqs)
             s_z = 0
             scale = [s_x, s_y, s_z]
@@ -277,19 +301,59 @@ class SignalWidget(QWidget):
 
             self.spectrogram.transform = transform
 
-            r = self.spectrum_camera.rect
-            r.left = 0
-            r.right = n_windows
-            r.bottom = 0
-            r.top = (self.curr_pc.fsamp / 2)
-            self.spectrum_camera.rect = r
+            self.spectrum_camera.rect = (0, 0), (n_windows * s_x,
+                                                 self.curr_pc.fsamp / 2)
 
 
-class ToolsWidget(QWidget):
+class ToolsWidget(QTabWidget):
 
     def __init__(self, parent):
         super(ToolsWidget, self).__init__(parent)
-        pass
+
+        # TODO - input masks for edit fields
+
+        self.spectrum_tab = QWidget()
+
+        layout = QFormLayout()
+        self.low_lim_le = QLineEdit()
+        self.high_lim_le = QLineEdit()
+        layout.addRow("Low limit", self.low_lim_le)
+        layout.addRow("High limit", self.high_lim_le)
+
+        self.low_lim_le.editingFinished.connect(self.set_low_lim)
+        self.high_lim_le.editingFinished.connect(self.set_high_lim)
+
+        self.spectrum_tab.setLayout(layout)
+
+        self.spectrogram_tab = QWidget()
+
+        self.addTab(self.spectrum_tab, 'Spectrum')
+        self.addTab(self.spectrogram_tab, 'Spectrogram')
+
+        self.currentChanged.connect(self.switch_spect_type)
+
+    def switch_spect_type(self):
+
+        if self.tabText(self.currentIndex()) == 'Spectrum':
+            self.parent().signal_widget.set_spect_type('spectrum')
+        elif self.tabText(self.currentIndex()) == 'Spectrogram':
+            self.parent().signal_widget.set_spect_type('spectrogram')
+
+    def set_low_lim(self):
+        if self.low_lim_le.text() != '':
+            self.parent().signal_widget.low_lim = int(self.low_lim_le.text())
+            self.parent().signal_widget.update_signals()
+        else:
+            self.parent().signal_widget.low_lim = 0
+            self.parent().signal_widget.update_signals()
+
+    def set_high_lim(self):
+        if self.high_lim_le.text() != '':
+            self.parent().signal_widget.high_lim = int(self.high_lim_le.text())
+            self.parent().signal_widget.update_signals()
+        else:
+            self.parent().signal_widget.high_lim = 0
+            self.parent().signal_widget.update_signals()
 
 
 class Measurement(BasePluginWidget):
