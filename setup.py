658f59ad9f8b967bedbb00faa08963660561b5fb
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2018-12-19 16:40:29 +0100

    version change

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ if os.name == 'nt':
 # =============================================================================
 
 setup_args = dict(name='pysigview',
-                  version='0.0a7',
+                  version='0.0a8',
                   description='Package for viewing signals using VisPy',
                   url='https://github.com/ICRC-BME/PySigView',
                   author='Jan Cimbalnik',
